9c469f4c13433ac9d4344946d0e1b05d70db7115
Ken Cochrane <kencochrane@gmail.com>
2012-01-13 11:39:01 -0500

    trying to fix egg caching issue take 9

--- wsgi/mycms/settings.py
+++ wsgi/mycms/settings.py
@@ -29,10 +29,10 @@ if ON_OPENSHIFT:
         'default': {
             'ENGINE': 'django.db.backends.mysql',  # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
             'NAME': 'blog',  # Or path to database file if using sqlite3.
-            'USER': os.environ['OPENSHIFT_DB_USERNAME'],                      # Not used with sqlite3.
+            'USER': 'admin', #os.environ['OPENSHIFT_DB_USERNAME'],                      # Not used with sqlite3.
             'PASSWORD': os.environ['OPENSHIFT_DB_PASSWORD'],                  # Not used with sqlite3.
-            'HOST': os.environ['OPENSHIFT_DB_HOST'],                      # Set to empty string for localhost. Not used with sqlite3.
-            'PORT': "",                      # Set to empty string for default. Not used with sqlite3.
+            'HOST': '127.1.30.1', #os.environ['OPENSHIFT_DB_HOST'],                      # Set to empty string for localhost. Not used with sqlite3.
+            'PORT': "3306",                      # Set to empty string for default. Not used with sqlite3.
         }
     }
 else:
