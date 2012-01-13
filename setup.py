498a3e8c3ad7012fbf994e3bac7d933032b9f5a5
Ken Cochrane <kencochrane@gmail.com>
2012-01-13 10:59:45 -0500

    adding mysql lib to dependencies

--- setup.py
+++ setup.py
@@ -9,5 +9,9 @@ setup(
     author='Your Name',
     author_email='example@example.com',
     url='http://www.python.org/sigs/distutils-sig/',
-    install_requires=['Django>=1.3', 'PIL==1.1.7', 'django-cms==2.2', 'south==0.7.2'],
+    install_requires=['Django>=1.3', 
+                      'PIL==1.1.7', 
+                      'django-cms==2.2', 
+                      'south==0.7.2', 
+                      'MySQL-python==1.2.3'],
 )
