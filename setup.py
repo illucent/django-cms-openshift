253a4807e74fa7d7c3064e618630fbb519886888
illucent <soho@illucent.info>
2016-01-06 13:43:57 +0200

    django-cms requirements

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
+# -*- encoding: utf-8 -*-
 
+
+# http://docs.django-cms.org/en/develop/how_to/install.html
 from setuptools import setup
 
 setup(
@@ -9,10 +12,15 @@ setup(
     author='Ken Cochrane',
     author_email='example@example.com',
     url='http://www.python.org/sigs/distutils-sig/',
-    install_requires=['Django>=1.3',
+    install_requires=['Django>=1.8',
+                      'django-classy-tags>=0.6.2',
+                      'django-treebeard==3.0',
+                      'django-sekizai>=0.82',
+                      'djangocms-admin-style',
+                      'html5lib>=0.9999999',
                       'PIL==1.1.7',
                       'django-cms==3.2',
-                      'south==0.7.2',
-                      'MySQL-python==1.2.3'
+                      'south>=1.0.2',
+                      'MySQL-python>=1.2.5'
                       ],
 )
